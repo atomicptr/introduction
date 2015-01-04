da661464eec6157a8215c20be453686f092ec6ef
Christopher Kaster <ikasoki@gmail.com>
2015-01-04 16:33:03 +0100

    use fullpage.js and create base lib

---
+++ js/introduction.js
@@ -0,0 +1,56 @@
+Introduction = function(elem) {
+    this._base = elem;
+
+    this._fullpage_settings = {
+        //Navigation
+        menu: false,
+        navigation: false,
+        navigationPosition: 'right',
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        scrollBar: true,
+        easing: 'easeInQuart',
+        easingcss3: 'ease',
+        continuousVertical: false,
+        scrollOverflow: false,
+        touchSensitivity: 15,
+        loopHorizontal: false,
+        normalScrollElementTouchThreshold: 5,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //Design
+        controlArrows: false,
+        verticalCentered: true,
+        resize : true,
+        responsive: 0,
+    };
+}
+
+Introduction.create = function(ident) {
+    $(ident).empty();
+
+    $(ident).append("<div class='section'></div>");
+
+    return new Introduction(ident);
+}
+
+Introduction.prototype.init = function() {
+    $(this._base).fullpage(this._fullpage_settings);
+}
+
+Introduction.prototype.addPage = function(ident) {
+    $(ident).addClass("slide");
+    var page = $(ident).detach();
+
+    page.appendTo(".section");
+}
